1942ab400036961c3b68b70d59a17c107a597c42
johnny <john_newcreek@protonmail.com>
2022-03-31 18:23:08 +0300

    added index.html and app.js, random number generator function 'computer'play added, started the game function

---
+++ app.js
@@ -0,0 +1,10 @@
+const computerPlay = () => {
+    let computerSelection = Math.floor(Math.random()* 4)
+    return computerSelection;
+};
+console.log(num);
+
+const play = (playerSelection, computerSelection) => {
+    
+    
+}
