67125533b6a1df57f908c2686521efd3269a81b6
johnny <john_newcreek@protonmail.com>
2022-04-06 16:28:40 +0300

    added game function with game logic which sets a result (win, lose, tie)

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const choices = document.querySelectorAll("button");
 let choiceArray = ['rock', 'paper', 'scissors'];
+let result = '';
 let userChoice;
-let computerChoice = choiceArray[Math.floor(Math.random() * 3)];
-// console.log(computerChoice);
+let computerChoice;
 
 choices.forEach(choice => choice.addEventListener('click', e => {
+    computerChoice = choiceArray[Math.floor(Math.random() * 3)];
     userChoice = e.target.id
-    // console.log(userChoice);
-}))
+    game(userChoice,computerChoice)
+    console.log(result);
+}));
 
+function game (userChoice, computerChoice) {
+    console.log(userChoice);
+    console.log(computerChoice);
+    if((userChoice == 'rock' && computerChoice == 'scissors') || (userChoice == 'paper' && computerChoice == 'rock') || (userChoice == 'scissors' && computerChoice == 'paper')) {
+        result = "you WIN this round!"
+    }
+    if((userChoice == 'scissors' && computerChoice == 'rock')|| (userChoice == 'rock' && computerChoice == 'paper') || (userChoice == 'paper' && computerChoice === 'scissors')) {
+        result = "you LOSE this round!"
+    }
+    else if (userChoice == computerChoice){
+        result = "this round is a TIE!"
+    }
+}
