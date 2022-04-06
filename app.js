1d9a38436fa233f77ae8d163bfbc0abf2247bb60
johnny <john_newcreek@protonmail.com>
2022-04-06 15:58:07 +0300

    added random choice for computer

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const choices = document.querySelectorAll("button");
+let choiceArray = ['rock', 'paper', 'scissors'];
+let userChoice;
+let computerChoice = choiceArray[Math.floor(Math.random() * 3)];
+// console.log(computerChoice);
 
 choices.forEach(choice => choice.addEventListener('click', e => {
-    console.log("hello")
+    userChoice = e.target.id
+    // console.log(userChoice);
 }))
+
